7a4954ef1a339ececdbe453f0f3bd88a955c1b87
Dave Kanter <moatgoat@hotmail.com>
2019-07-10 12:18:01 -0700

    Configuring Firelink.

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-flamelink',
+      options: {
+        firebaseConfig: {
+          pathToServiceAccount: './beyond-napa-valley-firebase-adminsdk-bs9lb-ef823287e3.json',
+          databaseURL: 'https://beyond-napa-valley.firebaseio.com',
+          storageBucket: 'gs://beyond-napa-valley'
+        },
+        dbType: 'cf',
+        environment: 'production',
+        content: true,
+        navigation: true,
+        globals: true
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
